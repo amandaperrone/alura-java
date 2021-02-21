55bff8111eac5bb3f60e8ec78f460065eda79796
Amanda Perrone <64797685+amandaperrone@users.noreply.github.com>
2021-02-21 20:51:04 -0300

    Delete bytebank-herdado directory

--- bytebank-herdado/src/Designer.java
+++
@@ -1,8 +0,0 @@
-public class Designer extends Funcionario {
-	
-	public double getBonificacao() {
-		System.out.println("Chamando método getBonificacao - classe Designer");
-		return 200;
-	}
-	 
-}
--- bytebank-herdado/src/EditorVideo.java
+++
@@ -1,9 +0,0 @@
-public class EditorVideo extends Funcionario {
-	
-	public double getBonificacao() {
-		System.out.println("Chamando método getBonificacao - classe editorVideo");
-		//return super.getBonificacao() + 100;
-		return 150;
-	}
-	 
-}
